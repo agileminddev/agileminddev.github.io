32f59ea70cc38d682c2f2032e5a65eecb93da624
Michael Klobe <mklobe@agilemind.com>
2023-11-30 21:10:19 -0600

    better docs; disable keypad appropriately

--- timer/script.js
+++ timer/script.js
@@ -1,8 +1,13 @@
+// string to use when resetting
 let resetString = null;
+// current timer
 let timerString = "";
+// running timer
 let active = null;
+// true when timer hits zero but before acked
 let alarming = false;
 
+// convert timer string into minutes and seconds
 const getTimerValue = () => {
   const paddedTimerString = `0000${timerString}`.slice(-4);
   const mins = parseInt(paddedTimerString.slice(0, 2));
@@ -10,21 +15,27 @@ const getTimerValue = () => {
   return [mins, secs];
 }
 
+// format given digit
 const formatDigit = (id, index) => {
+  // pluck digit from string
   let timerDigit = "";
   if (timerString.length > index) {
     const reversed = timerString.split("").reverse().join("");
     timerDigit = reversed.slice(index, index + 1);
   }
+  // was it entered or not?
   const set = timerDigit.length > 0;
 
+  // update the proper digit on the screen
   const digitSpan = document.getElementById(id);
   if (set) {
+    // digit entered by user
     if (!id.endsWith("-label")) {
       digitSpan.innerHTML = timerDigit;
     }
     digitSpan.classList.remove("faded-fg");
   } else {
+    // placeholder digit
     if (!id.endsWith("-label")) {
       digitSpan.innerHTML = "0";
     }
@@ -32,6 +43,7 @@ const formatDigit = (id, index) => {
   }
 }
 
+// redraw the timer
 const formatClock = () => {
   formatDigit("min-tens", 3);
   formatDigit("min-ones", 2);
@@ -42,6 +54,7 @@ const formatClock = () => {
   updateActiveButtons();
 }
 
+// hide or show the element
 const toggleElementVisibility = (id, display) => {
   const element = document.getElementById(id);
   if (element.style.display == "none") {
@@ -51,16 +64,19 @@ const toggleElementVisibility = (id, display) => {
   }
 }
 
+// is the element shown?
 const isElementVisible = (id) => {
   const element = document.getElementById(id);
   return element.style.display != "none"
 }
 
+// show/hide the keypad section
 const toggleTenkey = () => {
   toggleElementVisibility("divider");
   toggleElementVisibility("tenkey", "grid");
 }
 
+// a digit was pushed
 const tenkeyPush = (value) => {
   if (active) {
     return;
@@ -72,13 +88,8 @@ const tenkeyPush = (value) => {
   formatClock();
 }
 
+// start the countdown bar
 const startAnimation = (seconds) => {
-  if (seconds > 0) {
-    console.log(`----starting animation: ${seconds}s`);
-  } else {
-    console.log("----restarting animation");
-  }
-
   let countdown = document.getElementById("countdown");
   if (seconds > 0) {
     // create and replace element to actually restart animation
@@ -90,14 +101,15 @@ const startAnimation = (seconds) => {
   countdown.style["animation-play-state"] = "running";
 }
 
+// stop reset the animation
 const resetAnimation = () => {
-  console.log("----reset animation");
   const countdown = document.getElementById("countdown");
   countdown.style["animation-duration"] = "unset";
   countdown.style["animation-play-state"] = "paused";
   countdown.style["animation-fill-mode"] = "none";
 }
 
+// backspace or delete was pushed
 const backspacePush = () => {
   if (timerString == "" || active) {
     return;
@@ -109,6 +121,7 @@ const backspacePush = () => {
   formatClock();
 }
 
+// escape key was pushed
 const escapePush = () => {
   if (active) {
     return;
@@ -120,6 +133,7 @@ const escapePush = () => {
   formatClock();
 }
 
+// initialize audio
 let audioElement = null;
 let audioContext = null;
 const initAudio = () => {
@@ -131,8 +145,8 @@ const initAudio = () => {
   audioContext.createMediaElementSource(audioElement).connect(audioContext.destination);
 }
 
+// active timer just hit zero
 const timerExpired = () => {
-  console.log('----------timerExpired');
   alarming = true;
   document.getElementById("start").innerHTML = "OK";
   document.getElementById("clock").classList.add("alarming-bg");
@@ -149,6 +163,7 @@ const timerExpired = () => {
   formatClock();
 }
 
+// tick is the method that gets called every second
 var startTime = 0;
 const tick = () => {
   startTime = new Date().getTime();
@@ -173,15 +188,22 @@ const tick = () => {
   }
 }
 
+// stop button was hit
 var timeLeft = 0;
 const stopTimer = (startButton) => {
-  console.log('----------stopTimer');
+  // update label
   const button = startButton || document.getElementById("start");
   button.innerHTML = "Start";
+
+  // cancel 1 second timer
   clearTimeout(active);
-  timeLeft = 1000 - (new Date().getTime() - startTime);
   active = null;
+
+  // figure out how many millis left in the current second if need to restart
+  timeLeft = 1000 - (new Date().getTime() - startTime);
+
   if (alarming) {
+    // the alarm is going off -- shut it all down
     audioElement.pause();
     audioContext.suspend();
     audioElement.currentTime = 0
@@ -194,43 +216,53 @@ const stopTimer = (startButton) => {
     document.getElementById("sec-tens").classList.remove("alarming-fg");
     formatClock();
   }
+
+  // pause the countdown bar
   document.getElementById("countdown").style["animation-play-state"] = "paused";
   document.getElementById("countdown-wrapper").classList.add("faded-bg");
 }
 
+// start/stop/ok button pushed
 const startPush = (startButton) => {
-  console.log('----------start');
-  if (active || alarming) {
-    if (alarming) {
-      resetAnimation();
-    }
+  if (alarming) {
+    // alarm is going off
+    resetAnimation();
+    stopTimer(startButton);
+  } else if (active) {
+    // pause the timer
     stopTimer(startButton);
   } else {
+    // nothing else is happening -- start the timer
     const [mins, secs] = getTimerValue();
     if (mins + secs == 0) {
+      // okay, the user hasn't entered any time so nothing to do
       return;
     }
 
-    const countdown = document.getElementById("countdown");
+    // how many seconds on the timer?
     let seconds = 0;
     if (!resetString) {
+      // remember the starting point if there isn't one
       resetString = timerString;
       seconds = mins * 60 + secs;
     }
+    // restart the timer
     startAnimation(seconds);
     startButton.innerHTML = "Stop";
+    const countdown = document.getElementById("countdown");
     document.getElementById("countdown-wrapper").classList.remove("faded-bg");
     if (timeLeft < 1) {
+      // if the millis from the last go-around are close to zero just restart at a second
       timeLeft = 1000;
     }
-    console.log(`-----starting with ${timeLeft}ms`);
+    // start timer chain
     active = setTimeout(tick, timeLeft);
     timeLeft = 0;
   }
 }
 
+// reset button pushed
 const resetPush = () => {
-  console.log('----------reset');
   stopTimer();
   if (resetString) {
     resetAnimation();
@@ -240,30 +272,39 @@ const resetPush = () => {
   }
 }
 
+// make big or small
 const toggleSize = () => {
   const clock = document.getElementById("clock");
   clock.classList.toggle("big");
 }
 
+// enable/disable start/reset buttons
 const updateActiveButtons = () => {
   const [mins, secs] = getTimerValue();
   document.getElementById("start").disabled = mins + secs == 0 && !alarming;
   document.getElementById("reset").disabled = !resetString;
+  // should probably enable/disable tenkey as well
+  const keys = [27, 46, ...Array.from({ length: 10 }, (_, index) => 48 + index)];
+  for (const key of keys) {
+    document.getElementById(`key-${key}`).disabled = alarming || active;
+  }
 }
 
+// initialize listeners
 const setupListeners = () => {
-  // watch for tenkey from keyboard
+  // watch for input from keyboard
   document.addEventListener("keydown", (event) => {
-    console.log(event);
     const code = event.keyCode == 8 ? 46 : event.keyCode; // 8 and 46 are backspace/delete
+
+    // 0-9 named for the keycodes
     const key = document.getElementById(`key-${code}`);
     if (key && !active) {
       key.click();
     }
-    if ("BUTTON" != document.activeElement.tagName) {
-      if (code == 13) {
-        startPush(document.getElementById("start"));
-      }
+
+    // if return pressed when a button doesn't have the focus
+    if ("BUTTON" != document.activeElement.tagName && code == 13) {
+      startPush(document.getElementById("start"));
     }
   }, false);
 
